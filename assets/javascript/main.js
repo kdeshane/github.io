fec013ac14f8047fc1e5f558f9965d5a92c3bd6a
Keith Deshane <keith.deshane@xcentium.com>
2018-10-24 07:58:35 -0400

    remove from github.io

--- assets/javascript/main.js
+++
@@ -1,9 +0,0 @@
-// JavaScript source code
-
-$(document).ready(function () {
-    $('.employee').hover(function (e) {
-        $(e.currentTarget).find('.employee-quote').fadeIn();
-    }, function (e) {
-        $(e.currentTarget).find('.employee-quote').fadeOut();
-    });
-});
